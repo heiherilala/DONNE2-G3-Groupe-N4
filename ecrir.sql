c3d8b22f0d9efc4329e064f567d35046ac57d3fa
herilala <nyainaherilala@gmail.com>
2022-07-12 01:45:59 +0300

    insertiion complet exept maintenance

--- ecrir.sql
+++ ecrir.sql
@@ -1,34 +1,3 @@
---\i D:HEI/GitHub/DONNE2-G3-Groupe-N4/insertion.sql
-
-ALTER TABLE offre ADD label_offre varchar(20) not null;
-
-
-INSERT INTO type_payement(type_payement) VALUES('airtel_money'),('mvola'),('orange_money'),('carte'),('espece');
-
-
-INSERT INTO ville(nom_ville) VALUES('Antananarivo'),('Fianarantsoa'),('Majunga'),('Antsiranana'),('Tamatavy');
-
-
-INSERT INTO trajet(frais_de_base,prix_carburant,id_ville_depart,id_ville_arrivee) VALUES
-(30000,100000,1,2),(30000,100000,2,1),(30000,100000,3,1),(30000,100000,4,1),
-(30000,100000,5,1),(30000,100000,1,3),(30000,100000,1,4),(30000,100000,1,5);
-
-
-INSERT INTO saison(evenement,coef_saison) VALUES('basse_saison',1),('haute_saison',1.2);
-
-
-
-INSERT INTO offre(nombre_place,coef_offre,label_offre) VALUES(15,1,'normal'),(12,1.2,'premium'),(8,1.5,'vip');
-
-
-INSERT INTO incident(description_incident,depense_incident) VALUES
-('Empanne avec sur la route RN2, le moteur ne marche plus. Une autre voiture a dû prendre les passagers',200000),
-('Accident de voiture avec un camion BEN, 3 passagers blessés',5000000),
-('Le far droit ne marche plus, remplacement d’’ampoule laid',562000),
-('Passager malade durant le voyage, le voiture a du l’’emmener a l’optimale',100000);
-
-
-
 insert into chauffeur (id_chauffeur,nom_chauffeur,prenom_chauffeur,adresse_chauffeur,cin_chauffeur,telephone_chauffeur,salaire_chauffeur) values (1,'Valintiana','Mendrikay','imerintsiatosika','102-251-730-404','033-32-649-59',550000);
 insert into chauffeur (id_chauffeur,nom_chauffeur,prenom_chauffeur,adresse_chauffeur,cin_chauffeur,telephone_chauffeur,salaire_chauffeur) values (2,'Soandriaka','Nivo','farafangana','102-519-515-851','032-87-384-39',400000);
 insert into chauffeur (id_chauffeur,nom_chauffeur,prenom_chauffeur,adresse_chauffeur,cin_chauffeur,telephone_chauffeur,salaire_chauffeur) values (3,'Nambinintsoa','Mahony','miarinarivo','102-139-959-312','034-75-623-56',430000);
