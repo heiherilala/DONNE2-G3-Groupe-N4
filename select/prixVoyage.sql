1fbc360cbd259bccd7c760324f40a73d8653886b
herilala <nyainaherilala@gmail.com>
2022-07-18 18:52:41 +0300

    fix prixVoyage insert to use left join

--- select/prixVoyage.sql
+++ select/prixVoyage.sql
@@ -1,13 +1,17 @@
 -- prix du voyage (ici N°12) avec coefficient offre et coefficient saison
 
-Select saison.coef_saison, offre.coef_offre, trajet.frais_de_base,
- saison.coef_saison * offre.coef_offre * trajet.frais_de_base frai_trajet_total,
+Select coef_saison, coef_offre, frais_de_base, date_depart, horaire_depart,
+ coef_saison * coef_offre * frais_de_base frai_trajet_total,
  villeA.nom_ville ville_de_depart, villeB.nom_ville ville_d_arrivee 
-from voyage, saison, offre, trajet, voiture, ville villeA, ville villeB where 
-voyage.id_voiture = voiture.id_voiture and 
-voyage.id_voyage = 13 and 
-offre.id_offre = voiture.id_offre and
-villeA.id_ville = trajet.id_ville_depart and
-villeB.id_ville = trajet.id_ville_arrivee and
-voyage.id_trajet = trajet.id_trajet and
-voyage.id_saison = saison.id_saison;
+
+from voyage
+
+left join saison on voyage.id_saison = saison.id_saison 
+left join trajet on voyage.id_trajet = trajet.id_trajet 
+left join voiture on voyage.id_voiture = voiture.id_voiture
+left join offre on offre.id_offre = voiture.id_offre
+left join ville villeA on villeA.id_ville = trajet.id_ville_depart
+left join ville villeB on villeB.id_ville = trajet.id_ville_arrivee
+
+where 
+voyage.id_voyage = 13;
