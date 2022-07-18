92da523eb88d0918e22dddfc613f7f349afa4fbb
herilala <nyainaherilala@gmail.com>
2022-07-18 21:05:25 +0300

    add chercheVoyagePaDate in insert

---
+++ select/chercerVoyageParDate.sql
@@ -0,0 +1,20 @@
+-- prix du voyage (ici N°12) avec coefficient offre et coefficient saison
+
+Select date_depart, horaire_depart,
+ villeA.nom_ville ville_de_depart, 
+ villeB.nom_ville ville_d_arrivee,
+ immatriculation
+
+from voyage
+
+left join trajet on voyage.id_trajet = trajet.id_trajet 
+left join voiture on voyage.id_voiture = voiture.id_voiture 
+left join ville villeA on villeA.id_ville = trajet.id_ville_depart 
+left join ville villeB on villeB.id_ville = trajet.id_ville_arrivee 
+
+where 
+trajet.id_ville_depart = 1 and
+trajet.id_ville_arrivee = 2 and
+date_depart = '2022-02-18'
+
+;
--- select/nombereservationOffre.sql
+++ select/nombereservationOffre.sql
@@ -2,8 +2,9 @@
 --(combien on a eu de reservation premium, lite, et VIP) 
 --durant le dernier mois
 
-Select COUNT(id_voyage)  nombre_de_reservation_par_offre, 'LITE' nom_offre
-from voyage
+Select COUNT(id_reservation)  nombre_de_reservation_par_offre, 'LITE' nom_offre
+from reservation
+left join voyage on voyage.id_voyage = reservation.id_voyage
 left join voiture on voyage.id_voiture = voiture.id_voiture
 left join offre on offre.id_offre = voiture.id_offre
 where 
@@ -12,8 +13,9 @@ date_depart > current_date - INTERVAL '1 month'
 
 UNION
 
-Select COUNT(horaire_depart)  nombre_de_reservation_par_offre, 'PRENIUM' nom_offre
-from voyage
+Select COUNT(id_reservation)  nombre_de_reservation_par_offre, 'PRENIUM' nom_offre
+from reservation
+left join voyage on voyage.id_voyage = reservation.id_voyage
 left join voiture on voyage.id_voiture = voiture.id_voiture
 left join offre on offre.id_offre = voiture.id_offre
 where 
@@ -22,11 +24,12 @@ date_depart > current_date - INTERVAL '1 month'
 
 UNION
 
-Select COUNT(horaire_depart)  nombre_de_reservation_par_offre, 'VIP' nom_offre
-from voyage
+Select COUNT(id_reservation)  nombre_de_reservation_par_offre, 'VIP' nom_offre
+from reservation
+left join voyage on voyage.id_voyage = reservation.id_voyage
 left join voiture on voyage.id_voiture = voiture.id_voiture
 left join offre on offre.id_offre = voiture.id_offre
-where 
+where
 offre.id_offre = 3 and
 date_depart >= current_date - INTERVAL '1 month'
 ;
