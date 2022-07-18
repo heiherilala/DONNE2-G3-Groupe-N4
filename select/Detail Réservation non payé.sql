77c695b37bdf53aa109585d683cf8c6dab9ffd84
KennyRandria <103984550+KennyRandria@users.noreply.github.com>
2022-07-19 00:05:22 +0300

    Update and rename Detail Réservation non payé.sql to Details_Réservation_non_payé.sql

--- select/Detail Réservation non payé.sql
+++
@@ -1,4 +0,0 @@
--- details des réservations non payées
-
-Select COUNT(id_reservation) nombre_reservation_par_payement,net_a_payer from reservation
-left join payement on payement.id_payement where net_a_payer >0
