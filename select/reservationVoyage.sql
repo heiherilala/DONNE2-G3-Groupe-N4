00534a42d62c248770cd3cf39ec419bb6ae193db
herilala <nyainaherilala@gmail.com>
2022-07-19 04:00:23 +0300

    add reservationVoyage in insert

--- select/reservationVoyage.sql
+++ select/reservationVoyage.sql
@@ -1,13 +1,11 @@
--- Liste des reservations pas payées 
-select client.nom, client.prenom, 
+-- afficher toutes les reservation sur un voyage.
+select client.nom nom_client, client.prenom prenem_client, 
 reservation.id_reservation, reservation.date_reservation, numero_place, 
 date_depart, horaire_depart,
-receptionniste.id_receptionniste, 
-payement.payement_complet,
 villeD.nom_ville ville_depart, villeA.nom_ville ville_arrivee
 from reservation
 
-inner join payement on payement.id_reservation=reservation.id_reservation
+
 inner join client on client.id_client= reservation.id_client
 inner join  receptionniste on  receptionniste.id_receptionniste=reservation.id_receptionniste
 inner join voyage on voyage.id_voyage = reservation.id_voyage
@@ -15,6 +13,5 @@ inner join trajet on voyage.id_trajet = trajet.id_trajet
 inner join ville villeD on trajet.id_ville_depart = villeD.id_ville
 inner join ville villeA on trajet.id_ville_arrivee = villeA.id_ville
 
-where payement.payement_complet = false;
-
+where voyage.id_voyage = 5;
 
