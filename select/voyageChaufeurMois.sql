e0b2060a812dcbfb77b07fb04c7a526247721fb2
herilala <nyainaherilala@gmail.com>
2022-07-19 03:21:15 +0300

    add voyageChaufeurMois in insert

--- select/voyageChaufeurMois.sql
+++ select/voyageChaufeurMois.sql
@@ -1,4 +1,4 @@
---Afficher les voyages du mois dernier
+--Afficher les voyages des 3 dernier mois 
 
 Select frais_de_base, date_depart, horaire_depart,
  coef_saison * coef_offre * frais_de_base frai_trajet_avec_coff,
@@ -12,10 +12,13 @@ left join voiture on voyage.id_voiture = voiture.id_voiture
 left join offre on offre.id_offre = voiture.id_offre
 left join ville villeA on villeA.id_ville = trajet.id_ville_depart
 left join ville villeB on villeB.id_ville = trajet.id_ville_arrivee
+left join chauffeur on voyage.id_chauffeur = chauffeur.id_chauffeur
 
 where 
-
-EXTRACT(MONTH FROM date_depart) = EXTRACT(MONTH FROM current_date - INTERVAL '1 MONTH') 
+chauffeur.id_chauffeur=1 and
+(EXTRACT(MONTH FROM date_depart) = EXTRACT(MONTH FROM current_date - INTERVAL '1 MONTH') or
+EXTRACT(MONTH FROM date_depart) = EXTRACT(MONTH FROM current_date - INTERVAL '2 MONTH') or
+EXTRACT(MONTH FROM date_depart) = EXTRACT(MONTH FROM current_date - INTERVAL '3 MONTH'))
 
 order by date_depart DESC
 
